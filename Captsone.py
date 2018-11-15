63c06d597d8c0aeb66bb3c74428c969541204f75
brd3sd <45008900+brd3sd@users.noreply.github.com>
2018-11-15 13:39:21 -0500

    Add files via upload

---
+++ Captsone.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# This notebook will be used for the Capstone Project course.
+
+# In[3]:
+
+
+import pandas as pd 
+import numpy as np
+print('Hello Capstone Project Course!')
+
